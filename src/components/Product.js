1c62c37cf85b32b436a58bd09c5de003dada3a80
unknown <uzair6930@gmail.com>
2023-08-27 01:06:29 +0500

    Cart Contex Completed

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,13 @@
 import React , {useContext}from 'react';
 import {Link} from 'react-router-dom'
 import {BsPlus , BsEyeFill} from 'react-icons/bs'
+//import cart context
+import { CartContext } from '../contexts/CartContext';
+
 const Product = ({product}) => {
 
-  console.log(product)
+  const {addToCart}  = useContext(CartContext);
+
   const {id , image : {url} , name , price: { formatted_with_symbol }} = product;
   return( 
   <div>
@@ -14,8 +18,9 @@ const Product = ({product}) => {
           <img className='max-h-[240px] group-hover:scale-110 transition duration-300'src={url} alt=''/>
         </div>
           {/**button*/}
-        <div className='absolute top-6 -right-11 group-hover:right-1 p-2 flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300'>
-          <button>
+        <div className='absolute top-6 -right-11 group-hover:right-1 p-2 flex flex-col items-center 
+        justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300'>
+          <button onClick={() => addToCart(product, id)}>
             <div className='flex justify-center items-center text-whilte w-12 h-12 bg-red-500'>
               <BsPlus className='text-3x1'/>
             </div>
