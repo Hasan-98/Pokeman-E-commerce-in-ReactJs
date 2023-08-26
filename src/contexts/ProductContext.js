3b1bd7d590a4561186c5ea06db3dc8049af76e66
unknown <uzair6930@gmail.com>
2023-08-26 16:24:27 +0500

    First Commit

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, {createContext, useState, useEffect} from
+'react';
 
-const ProductContext = () => {
-  return <div>ProductContext</div>;
+//create context
+export const ProductContext = createContext();
+
+const ProductProvider = ({children}) => {
+  //products state
+  const [products, setProducts] = useState([]);
+  const headers = {
+    "X-Authorization": import.meta.env.VITE_APP_API_KEY_LIVE,
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+  };
+
+
+  //fetch Products
+  useEffect(()=> {
+    const fetchProducts= async () => {
+      const response = await fetch("https://api.chec.io/v1/products?limit=40", {
+        method: 'GET',
+        headers: headers,
+        });
+
+        const data = await response.json();
+        //console.log(data);
+    };
+    fetchProducts();
+  }, [])
+
+  return <ProductContext.Provider> {children} </ProductContext.Provider>;
 };
 
-export default ProductContext;
+export default ProductProvider;
